72b8feda4e3ae63acb0af66fb3a4ee088f4437e8
Noah-Josua Hartmann <noah.josua@posteo.de>
2024-05-07 11:02:39 +0200

    initial commit

---
+++ src/test/java/com/example/budgettrackerv1/BudgetTrackerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgettrackerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
