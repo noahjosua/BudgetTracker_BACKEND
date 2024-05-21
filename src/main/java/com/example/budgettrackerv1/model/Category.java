75b4a3e02fd3b6edc7361c8cc8faec93dab182fa
Noah-Josua Hartmann <noah.josua@posteo.de>
2024-05-21 12:20:56 +0200

    push MockData and add comments on category

--- src/main/java/com/example/budgettrackerv1/model/Category.java
+++ src/main/java/com/example/budgettrackerv1/model/Category.java
@@ -1,12 +1,14 @@
 package com.example.budgettrackerv1.model;
 
 public enum Category {
+    /* Income */
     SALARY,
     POCKET_MONEY,
     ALIMENT,
     CAPITAL_ASSETS,
     RENTAL,
 
+    /* Expenses */
     GROCERIES,
     DRUGSTORE,
     FREE_TIME,
