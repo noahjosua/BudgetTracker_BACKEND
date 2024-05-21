8c56fec01a661ae5679351ce0785273fc34a248c
Noah-Josua Hartmann <66725268+noahjosua@users.noreply.github.com>
2024-05-21 12:22:17 +0200

    Feat/se 96/noah (#2)
    * add spring-boot-starter-data-jpa and gson to build.gradle, add Category class, add constants, modify data.sql so that it can work with Date Objects instead of Strings, modify Models to have Dates instead of Strings and use Category instead of String, add date_created variable to Models, implement expense and income Controller, add unimplemented Service so that the Controller can "use" it
    
    * push MockData
    
    * push MockData and add comments on category

---
+++ src/main/java/com/example/budgettrackerv1/MockData.java
@@ -0,0 +1,21 @@
+package com.example.budgettrackerv1;
+
+public class MockData {
+    public static final String json1 = "{\"dateCreated\": \"2023-05-15\",\n" +
+            "    \"datePlanned\": \"2023-07-01\",\n" +
+            "    \"category\": \"RENT\",\n" +
+            "    \"description\": \"Stromrechnung für Mai\",\n" +
+            "    \"amount\": 78.25}";
+
+    public static final String json2 = "{\"dateCreated\": \"2023-04-20\",\n" +
+            "    \"datePlanned\": \"2023-06-10\",\n" +
+            "    \"category\": \"FREE_TIME\",\n" +
+            "    \"description\": \"Kino: Avengers: Endgame\",\n" +
+            "    \"amount\": 22.5}";
+
+    public static final String json3 = "{\"dateCreated\": \"2023-03-05\",\n" +
+            "    \"datePlanned\": \"2023-03-28\",\n" +
+            "    \"category\": \"GROCERIES\",\n" +
+            "    \"description\": \"Wocheneinkauf bei Aldi\",\n" +
+            "    \"amount\": 65.75}";
+}
