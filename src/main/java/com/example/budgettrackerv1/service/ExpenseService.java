e0e330d80b85454f25e3e0c398cd970274af4b70
MissAvery <106031385+MissAvery@users.noreply.github.com>
2024-05-22 15:16:12 +0200

    Repositories and Services added

--- src/main/java/com/example/budgettrackerv1/service/ExpenseService.java
+++ src/main/java/com/example/budgettrackerv1/service/ExpenseService.java
@@ -1,7 +1,57 @@
 package com.example.budgettrackerv1.service;
 
+import com.example.budgettrackerv1.model.Expense;
+import com.example.budgettrackerv1.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    @Autowired
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public List<Expense> getExpenses(){
+        return this.expenseRepository.findAll();
+    }
+
+    public void save(Expense expense){
+        this.expenseRepository.save(expense);
+    }
+
+    public void delete(int id){
+        this.expenseRepository.deleteById(id);
+    }
+
+    public Expense getById(int id){
+        return this.expenseRepository.findById(id).get();
+    }
+
+    public List<Expense> getByPlannedDate(Date date){
+        List<Expense> expensesByPlannedDate = new ArrayList<>();
+        for(Expense expense : this.expenseRepository.findAll()){
+            if(expense.getDatePlanned().equals(date)){
+                expensesByPlannedDate.add(expense);
+            }
+        }
+        return expensesByPlannedDate;
+    }
+
+    public List<Expense> getByCreatedDate(Date date){
+        List<Expense> expensesByCreatedDate = new ArrayList<>();
+        for(Expense expense : this.expenseRepository.findAll()){
+            if(expense.getDateCreated().equals(date)){
+                expensesByCreatedDate.add(expense);
+            }
+        }
+        return expensesByCreatedDate;
+    }
 }
--- src/main/java/com/example/budgettrackerv1/service/IncomeService.java
+++ src/main/java/com/example/budgettrackerv1/service/IncomeService.java
@@ -1,8 +1,58 @@
 package com.example.budgettrackerv1.service;
 
+import com.example.budgettrackerv1.model.Income;
+import com.example.budgettrackerv1.repository.IncomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class IncomeService {
+
+    private final IncomeRepository incomeRepository;
+
+    @Autowired
+    public IncomeService(IncomeRepository incomeRepository) {
+        this.incomeRepository = incomeRepository;
+    }
+
+    public List<Income> getIncomes(){
+        return this.incomeRepository.findAll();
+    }
+
+    public void save(Income income){
+        this.incomeRepository.save(income);
+    }
+
+    public void delete(int id){
+        this.incomeRepository.deleteById(id);
+    }
+
+    public Income getById(int id){
+        return this.incomeRepository.findById(id).get();
+    }
+
+    public List<Income> getByPlannedDate(Date date){
+        List<Income> incomesByPlannedDate = new ArrayList<>();
+        for(Income income : this.incomeRepository.findAll()){
+            if(income.getDatePlanned().equals(date)){
+                incomesByPlannedDate.add(income);
+            }
+        }
+        return incomesByPlannedDate;
+    }
+
+    public List<Income> getByCreatedDate(Date date){
+        List<Income> incomesByCreatedDate = new ArrayList<>();
+        for(Income income : this.incomeRepository.findAll()){
+            if(income.getDateCreated().equals(date)){
+                incomesByCreatedDate.add(income);
+            }
+        }
+        return incomesByCreatedDate;
+    }
 
 }
