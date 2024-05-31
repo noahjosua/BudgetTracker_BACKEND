654ea0f2fb021735ff5306ccbe36b234740511dc
Noah-Josua Hartmann <noah.josua@posteo.de>
2024-05-31 17:07:25 +0200

    fix MockData and add methods to test db interaction

--- src/main/java/com/example/budgettrackerv1/controller/IncomeController.java
+++ src/main/java/com/example/budgettrackerv1/controller/IncomeController.java
@@ -1,14 +1,17 @@
 package com.example.budgettrackerv1.controller;
 
 import com.example.budgettrackerv1.Constants;
+import com.example.budgettrackerv1.MockData;
 import com.example.budgettrackerv1.model.Income;
 import com.example.budgettrackerv1.service.IncomeService;
 import com.google.gson.Gson;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = Constants.ALLOWED_ORIGIN)
@@ -22,6 +25,48 @@ public class IncomeController {
     public IncomeController(IncomeService INCOME_SERVICE) {
         this.INCOME_SERVICE = INCOME_SERVICE;
     }
+
+    @PostConstruct
+    public void init() {
+        /* Nur zum testen - sollte wieder gelöscht werden */
+        readAllIncomesFromDB();
+        saveIncomesToDB();
+        editIncomeInDB();
+        deleteIncomeFromDB();
+    }
+
+    /* Nur zum sicherstellen, dass DB Zugriff funktioniert - sollte wieder gelöscht werden */
+    public void readAllIncomesFromDB() {
+        System.out.println("#### INCOME CONTROLLER - READ FROM DB ####");
+        List<Income> incomes = this.INCOME_SERVICE.getIncomes();
+        for (Income income : incomes) {
+            System.out.println(income);
+        }
+    }
+
+    public void saveIncomesToDB() {
+        System.out.println("#### INCOME CONTROLLER - SAVE TO DB ####");
+        Income income = this.gson.fromJson(MockData.json2, Income.class);
+        System.out.println(income);
+        this.INCOME_SERVICE.save(income);
+        System.out.println(income);
+    }
+
+    public void editIncomeInDB() {
+        System.out.println("#### INCOME CONTROLLER - EDIT TO DB ####");
+        Income incomeToEdit = this.INCOME_SERVICE.getById(1);
+        System.out.printf("Income to edit: %s.%n", incomeToEdit);
+        incomeToEdit.setAmount(300.00);
+        System.out.printf("Save updated income %s.%n", incomeToEdit);
+        this.INCOME_SERVICE.save(incomeToEdit);
+    }
+
+    public void deleteIncomeFromDB() {
+        System.out.println("#### INCOME CONTROLLER - DELETE FROM DB ####");
+        Income income = this.INCOME_SERVICE.getById(1);
+        this.INCOME_SERVICE.delete(income.getId());
+    }
+    /* Ende */
 
     @GetMapping("incomes")
     public ResponseEntity<String> getAllIncomes() {
