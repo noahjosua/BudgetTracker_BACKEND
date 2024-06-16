f89e7663d3f839d95d7eb8fcab1970b7b26d3836
Noah-Josua Hartmann <noah.josua@posteo.de>
2024-06-16 22:00:42 +0200

    change allowed origin

--- src/main/java/com/example/budgettrackerv1/Constants.java
+++ src/main/java/com/example/budgettrackerv1/Constants.java
@@ -1,7 +1,7 @@
 package com.example.budgettrackerv1;
 
 public class Constants {
-    public static final String ALLOWED_ORIGIN = "http://localhost:4200";
+    public static final String ALLOWED_ORIGIN = "https://singular-piroshki-64cdbc.netlify.app/";
     public static final String REQUEST_MAPPING_EXPENSE = "/api/expenses";
     public static final String REQUEST_MAPPING_INCOME = "/api/incomes";
     public static final String RESPONSE_MESSAGE_KEY = "message";
