cfd1ec77e9032f3a66202f550664926639f7fd29
Michelle <116007525+mimbies@users.noreply.github.com>
2024-06-25 09:35:31 +0200

    added WebConfig and OpenAoiCOnfig (#18)

--- src/main/java/com/example/budgettrackerv1/config/OpenApiConfig.java
+++ src/main/java/com/example/budgettrackerv1/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.example.budgettrackerv1;
+package com.example.budgettrackerv1.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
