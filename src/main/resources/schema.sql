2f4d568ae5b026a56ce8321fbb2377769d462e9a
Noah-Josua Hartmann <66725268+noahjosua@users.noreply.github.com>
2024-07-04 08:55:31 +0200

    fix schema.sql (#23)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,30 +2,29 @@ DROP TABLE IF EXISTS income;
 
 CREATE TABLE IF NOT EXISTS income
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    date_created DATE NOT NULL,
-    date_planned DATE NOT NULL,
-    category    VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    amount      DECIMAL(10, 2)
-    );
-
-/*
-INSERT INTO income (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'SALARY', 'Test 1', 2.50);
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    date_created DATE         NOT NULL,
+    date_planned DATE         NOT NULL,
+    category     VARCHAR(255) NOT NULL,
+    description  VARCHAR(255) NOT NULL,
+    amount       DECIMAL(10, 2)
+);
 
 INSERT INTO income (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'POCKET_MONEY', 'Test 1', 2.50);
+VALUES (CURRENT_DATE, CURRENT_DATE, 'SALARY', 'Test 1', 2.50);
 
 INSERT INTO income (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'ALIMENT', 'Test 3', 2.50);
+VALUES (CURRENT_DATE, CURRENT_DATE, 'POCKET_MONEY', 'Test 1', 2.50);
 
 INSERT INTO income (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'CAPITAL_ASSETS', 'Test 4', 2.50);
+VALUES (CURRENT_DATE, CURRENT_DATE, 'ALIMENT', 'Test 3', 2.50);
 
 INSERT INTO income (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'RENTAL', 'Test 5', 2.50);
-*/
+VALUES (CURRENT_DATE, CURRENT_DATE, 'CAPITAL_ASSETS', 'Test 4', 2.50);
+
+INSERT INTO income (date_planned, date_created, category, description, amount)
+VALUES (CURRENT_DATE, CURRENT_DATE, 'RENTAL', 'Test 5', 2.50);
+
 
 /* EXPENSE */
 
@@ -33,27 +32,25 @@ DROP TABLE IF EXISTS expense;
 
 CREATE TABLE IF NOT EXISTS expense
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    date_created DATE NOT NULL,
-    date_planned DATE NOT NULL,
-    category    VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    amount      DECIMAL(10, 2)
-    );
-
-/*
-INSERT INTO expense (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'GROCERIES', 'Buch 1', 2.50);
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    date_created DATE         NOT NULL,
+    date_planned DATE         NOT NULL,
+    category     VARCHAR(255) NOT NULL,
+    description  VARCHAR(255) NOT NULL,
+    amount       DECIMAL(10, 2)
+);
 
 INSERT INTO expense (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'DRUGSTORE', 'Buch 2', 2.50);
+VALUES (CURRENT_DATE, CURRENT_DATE, 'GROCERIES', 'Buch 1', 2.50);
 
 INSERT INTO expense (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'FREE_TIME', 'Buch 3', 2.50);
+VALUES (CURRENT_DATE, CURRENT_DATE, 'DRUGSTORE', 'Buch 2', 2.50);
 
 INSERT INTO expense (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'RENT', 'Buch 4', 2.50);
+VALUES (CURRENT_DATE, CURRENT_DATE, 'FREE_TIME', 'Buch 3', 2.50);
 
 INSERT INTO expense (date_planned, date_created, category, description, amount)
-VALUES (CURDATE(), CURDATE(), 'EDUCATION', 'Buch 5', 2.50);
-*/
+VALUES (CURRENT_DATE, CURRENT_DATE, 'RENT', 'Buch 4', 2.50);
+
+INSERT INTO expense (date_planned, date_created, category, description, amount)
+VALUES (CURRENT_DATE, CURRENT_DATE, 'EDUCATION', 'Buch 5', 2.50);
